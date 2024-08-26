5d736e80a9e9add14e5169feec5a6d7841d2d080
redfyel <lianis.ee7@gmail.com>
2024-08-26 14:49:14 +0530

    added the structure for backend

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>AcademiaConnect - Your One Stop Solution for Academics</h1>
+
+      <p>Under Construction....</p>
+
+      <p>Visit us soon!</p>
+    </>
+  );
+}
+
+export default App;
