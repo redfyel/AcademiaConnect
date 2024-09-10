f617400c31380f97df28879517ede0bb814eba36
allurimeenakshi <meena.alluri2004@gmail.com>
2024-09-10 20:06:09 +0530

    Changes in Exam Corner

--- client/src/components/exam-corner/ExamCorner.jsx
+++ client/src/components/exam-corner/ExamCorner.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import './ExamCorner.css';
 
-function ExamCorner() {
+const CardLayout = () => {
+  const cards = [
+    {
+      title: 'TimeTable',
+      imgSrc: 'https://th.bing.com/th/id/OIP.iviJzwY87xQSLMLQ6G7tWgHaE-?pid=ImgDet&w=164&h=110&c=7&dpr=1.5',
+      link: '/time-table'
+    },
+    {
+      title: 'Syllabus',
+      imgSrc: 'https://th.bing.com/th/id/OIP.UFKXZSq14XRFIfhDm-NAhQHaF5?pid=ImgDet&w=164&h=130&c=7&dpr=1.5',
+      link: '/syllabus'
+    },
+    {
+      title: 'Results',
+      imgSrc: 'https://th.bing.com/th/id/OIP._BQGGZoKzvcKDiZkxY54EgHaH0?pid=ImgDet&w=164&h=174&c=7&dpr=1.5',
+      link: '/results'
+    },
+    {
+      title: 'Previous Papers',
+      imgSrc: 'https://th.bing.com/th/id/OIP.kSXQUXvLTAxdh_KRk9SrrQHaE7?pid=ImgDet&w=164&h=110&c=7&dpr=1.5',
+      link: '/previous-papers'
+    },
+  ];
+
   return (
-    <div>ExamCorner</div>
-  )
-}
+    <div className="card-layout">
+      {cards.map((card, index) => (
+        <div className="card" key={index}>
+          <img src={card.imgSrc} alt={card.title} className="card-image" />
+          <h3 className="card-title">{card.title}</h3>
+          <a href={card.link} className="card-button">
+            {card.title === 'TimeTable' ? 'View Time Table' : 'Explore'}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default ExamCorner
+export default CardLayout;
