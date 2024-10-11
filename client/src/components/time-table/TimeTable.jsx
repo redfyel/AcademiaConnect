8eadfb946f75e7a612c26a4ec3d6bf2b5502b024
allurimeenakshi <meena.alluri2004@gmail.com>
2024-10-11 19:34:24 +0530

    TimeTable

--- client/src/components/time-table/TimeTable.jsx
+++ client/src/components/time-table/TimeTable.jsx
@@ -1,52 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TimeTable.css';
 
-// Reusable TableComponent that takes data and title as props
-const TableComponent = ({ tableData, title }) => {
-  return (
-    <div className="table-container">
-      <h2 className="table-title">{title}</h2> {/* Title for the table */}
-      <table className="custom-table">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Subject Name</th>
-            <th>Tutorials</th>
-            <th>PYQ's</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.map((row, index) => (
-            <tr key={index}>
-              <td>{row.date}</td>
-              {/* Subject name linked to the syllabus */}
-              <td>
-                <a href={row.syllabus} title={`View the syllabus for ${row.subject}`} target='_blank'>
-                  {row.subject}
-                </a>
-              </td>
-              <td>
-                <a href={row.tutorials} title={`Go to ${row.subject} tutorials`}>
-                  {row.tutorials}
-                </a>
-              </td>
-              <td>
-                <a href={row.pyqs} title={`Go to ${row.subject} PYQs`}>
-                  {row.pyqs}
-                </a>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+const TimeTable = () => {
+  const [selectedTable, setSelectedTable] = useState(1);
 
-// Main component that wraps all TableComponents
-const TablesWrapper = () => {
-  // Data for each table
-  const tableContents = [
+  const handleTableChange = (tableNumber) => {
+    setSelectedTable(tableNumber);
+  };
+
+  const tablesData = [
     {
       title: 'I B.TECH - I SEMESTER',
       data: [
@@ -58,7 +20,8 @@ const TablesWrapper = () => {
         { date: '2024-09-11', subject: 'Communicative English I Lab', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/11/cse/20HS1151.pdf', tutorials: '/tutorials/dbms', pyqs: '/pyqs/dbms' },
         { date: '2024-09-10', subject: 'Engineering Physics Lab', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/11/cse/20BS1152.pdf', tutorials: '/tutorials/software', pyqs: '/pyqs/software' },
         { date: '2024-09-11', subject: 'Basic Electrical & Electronics Engineering Lab', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/11/cse/20ES1151.pdf', tutorials: '/tutorials/dbms', pyqs: '/pyqs/dbms' },
-      ],
+
+      ]
     },
     {
       title: 'I B.TECH - II SEMESTER',
@@ -72,7 +35,8 @@ const TablesWrapper = () => {
         { date: '2024-09-12', subject: 'Engineering Chemistry Lab', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/12/cse/20BS1251.pdf', tutorials: '/tutorials/os', pyqs: '/pyqs/os' },
         { date: '2024-09-13', subject: 'Programming for Problem Solving Lab', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/12/cse/20ES1253.pdf', tutorials: '/tutorials/ds', pyqs: '/pyqs/ds' },
         { date: '2024-09-12', subject: 'Life Sciences for Engineers', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/12/cse/20MC1201.pdf', tutorials: '/tutorials/os', pyqs: '/pyqs/os' },
-      ],
+
+      ]
     },
     {
       title: 'II B.TECH - I SEMESTER',
@@ -88,22 +52,22 @@ const TablesWrapper = () => {
       { date: '2024-09-14', subject: 'Introduction to Linux Operating System',syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/21/cse/20S08355.pdf', tutorials: '/tutorials/algorithms', pyqs: '/pyqs/algorithms' },
       { date: '2024-09-15', subject: 'Environmental Sciences', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/21/cse/20MC1301.pdf', tutorials: '/tutorials/cn', pyqs: '/pyqs/cn' },
       { date: '2024-09-15', subject: 'Community Service Project', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/21/cse/20CS3391.pdf', tutorials: '/tutorials/cn', pyqs: '/pyqs/cn' }
-      ],
+      ]
     },
     {
       title: 'II B.TECH - II SEMESTER',
       data: [
         { date: '2024-09-16', subject: 'Formal Languages and Automata Theory', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20BS1403.pdf', tutorials: '/tutorials/ml', pyqs: '/pyqs/ml' },
-      { date: '2024-09-17', subject: 'Operating Systems', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20CS3401.pdf', tutorials: '/tutorials/ai', pyqs: '/pyqs/ai' },
-      { date: '2024-09-16', subject: 'Advanced Data Structures', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20CS3402.pdf', tutorials: '/tutorials/ml', pyqs: '/pyqs/ml' },
-      { date: '2024-09-17', subject: 'Design and Analysis of Algorithms', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20CS3403.pdf', tutorials: '/tutorials/ai', pyqs: '/pyqs/ai' },
-      { date: '2024-09-16', subject: 'Internet of Things', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20ES1402.pdf', tutorials: '/tutorials/ml', pyqs: '/pyqs/ml' },
-      { date: '2024-09-17', subject: 'Internet of Things Lab', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20ES1452.pdf', tutorials: '/tutorials/ai', pyqs: '/pyqs/ai' },
-      { date: '2024-09-16', subject: 'Advanced Data Structures through C++ Lab', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20CS3451.pdf', tutorials: '/tutorials/ml', pyqs: '/pyqs/ml' },
-      { date: '2024-09-17', subject: 'Design and Analysis of Algorithms Lab', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20CS3452.pdf', tutorials: '/tutorials/ai', pyqs: '/pyqs/ai' },
-      { date: '2024-09-16', subject: 'Programming with JAVA', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20SO8454.pdf', tutorials: '/tutorials/ml', pyqs: '/pyqs/ml' },
-      { date: '2024-09-17', subject: 'Advanced Python Programming', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/CSE/20CS6421.pdf', tutorials: '/tutorials/ai', pyqs: '/pyqs/ai' }
-      ],
+        { date: '2024-09-17', subject: 'Operating Systems', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20CS3401.pdf', tutorials: '/tutorials/ai', pyqs: '/pyqs/ai' },
+        { date: '2024-09-16', subject: 'Advanced Data Structures', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20CS3402.pdf', tutorials: '/tutorials/ml', pyqs: '/pyqs/ml' },
+        { date: '2024-09-17', subject: 'Design and Analysis of Algorithms', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20CS3403.pdf', tutorials: '/tutorials/ai', pyqs: '/pyqs/ai' },
+        { date: '2024-09-16', subject: 'Internet of Things', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20ES1402.pdf', tutorials: '/tutorials/ml', pyqs: '/pyqs/ml' },
+        { date: '2024-09-17', subject: 'Internet of Things Lab', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20ES1452.pdf', tutorials: '/tutorials/ai', pyqs: '/pyqs/ai' },
+        { date: '2024-09-16', subject: 'Advanced Data Structures through C++ Lab', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20CS3451.pdf', tutorials: '/tutorials/ml', pyqs: '/pyqs/ml' },
+        { date: '2024-09-17', subject: 'Design and Analysis of Algorithms Lab', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20CS3452.pdf', tutorials: '/tutorials/ai', pyqs: '/pyqs/ai' },
+        { date: '2024-09-16', subject: 'Programming with JAVA', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/cse/20SO8454.pdf', tutorials: '/tutorials/ml', pyqs: '/pyqs/ml' },
+        { date: '2024-09-17', subject: 'Advanced Python Programming', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/22/CSE/20CS6421.pdf', tutorials: '/tutorials/ai', pyqs: '/pyqs/ai' }
+      ]
     },
     {
       title: 'III B.TECH - I SEMESTER',
@@ -120,7 +84,8 @@ const TablesWrapper = () => {
       { date: '2024-09-15', subject: 'Summer Internship', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/31/cse/20CS3581A.pdf', tutorials: '/tutorials', pyqs: 'pyqs' },
       { date: '2024-09-15', subject: 'Constitution of India', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/31/cse/20MC1501.pdf', tutorials: '/tutorials', pyqs: 'pyqs' },
       { date: '2024-09-15', subject: 'Advanced Java Programming', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/31/CSE/20CS6522.pdf', tutorials: '/tutorials', pyqs: 'pyqs' }
-      ],
+
+      ]
     },
     {
       title: 'III B.TECH - II SEMESTER',
@@ -136,7 +101,8 @@ const TablesWrapper = () => {
       { date: '2024-09-23', subject: 'Mobile App Development', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/32/cse/20SA8651.pdf', tutorials: '/tutorials/nlp', pyqs: '/pyqs/nlp' },
       { date: '2024-09-22', subject: 'Universal Human Values', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/32/cse/20MC1602.pdf', tutorials: '/tutorials/data-mining', pyqs: '/pyqs/data-mining' },
       { date: '2024-09-21', subject: 'Data Visualization', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/32/CSE/Data%20Visualization.pdf', tutorials: '/tutorials/cloud', pyqs: '/pyqs/cloud' }
-      ],
+
+      ]
     },
     {
       title: 'IV B.TECH - I SEMESTER',
@@ -152,24 +118,57 @@ const TablesWrapper = () => {
       { date: '2024-09-24', subject: 'Sales Force Technologies', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/41/CSE/20SA8755.pdf', tutorials: '/tutorials/iot', pyqs: '/pyqs/iot' },
       { date: '2024-09-25', subject: 'Industrial/Research Internship', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/41/CSE/20CS3781B_C.pdf', tutorials: '/tutorials/blockchain', pyqs: '/pyqs/blockchain' },
       { date: '2024-09-24', subject: 'Advanced Mobile Application Development', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/41/CSE/ADVANCED%20MOBILE%20APP%20DEVELOPMENT.pdf', tutorials: '/tutorials/iot', pyqs: '/pyqs/iot' },
-      ],
+
+      ]
     },
     {
       title: 'IV B.TECH - II SEMESTER',
       data: [
         { date: '2024-09-24', subject: 'Project work, seminar and internship in industry', syllabus: 'https://www.pvpsiddhartha.ac.in/autonomous20/42/cse/.pdf', tutorials: '/tutorials/iot', pyqs: '/pyqs/iot' }
-      ],
+
+      ]
     }
   ];
 
-  
   return (
-    <div className="tables-wrapper container">
-      {tableContents.map((table, index) => (
-        <TableComponent key={index} title={table.title} tableData={table.data} />
-      ))}
+    <div className="time-table-app">
+      {/* Buttons */}
+      <div className="button-container">
+        {tablesData.map((_, index) => (
+          <button key={index} onClick={() => handleTableChange(index + 1)}>
+            Sem {index + 1}
+          </button>
+        ))}
+      </div>
+
+      {/* Tables */}
+      <div className="table-container">
+        <h2 className="table-title">{tablesData[selectedTable - 1].title}</h2>
+        <table className="custom-table">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Subject</th>
+              <th>Syllabus</th>
+              <th>Tutorials</th>
+              <th>PYQs</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tablesData[selectedTable - 1].data.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                <td>{row.date}</td>
+                <td>{row.subject}</td>
+                <td className="syllabus-link"><a href={row.syllabus} target="_blank" rel="noopener noreferrer">Syllabus</a></td>
+                <td className="tutorials-link"><a href={row.tutorials}>Tutorials</a></td>
+                <td className="pyqs-link"><a href={row.pyqs}>PYQs</a></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
 
-export default TablesWrapper;
+export default TimeTable;
