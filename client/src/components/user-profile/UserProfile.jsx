bdcedc4a2ad2cb8736cc85fb56c1968aabe9c719
chandrasekharchandana <chandrasekhar3626@gmail.com>
2024-10-19 16:58:17 +0530

    user profilr

--- client/src/components/user-profile/UserProfile.jsx
+++ client/src/components/user-profile/UserProfile.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Tracker from '../tracker/Tracker'; // Make sure the path is correct
+import StudentCorner from '../student-corner/StudentCorner';
+import EventCalendar from '../events/EventCalendar';
+import './UserProfile.css'; // Import CSS for styling
 
 function UserProfile() {
   return (
-    <div>UserProfile</div>
+    
+    <div>UserProfile
+      
+      <body>
+        <Tracker />
+        <StudentCorner />
+        <EventCalendar />
+      </body>
+    </div>
+
   )
 }
+
 
 export default UserProfile
