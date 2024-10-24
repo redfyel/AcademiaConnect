6ec7c819a981283ec4baae19e93875c5a8eb74ad
redfyel <lianis.ee7@gmail.com>
2024-10-25 01:49:30 +0530

    revamped event calendar UI, added gravatar functionality for profile, changed fonts, changes during login

--- client/src/RootLayout.jsx
+++ client/src/RootLayout.jsx
@@ -7,7 +7,7 @@ const Layout = () => {
   return (
     <div>
       <Header />
-      <div style={{minHeight : "80vh"}}>
+      <div style={{minHeight : "100vh"}}>
         <Outlet />
       </div>
       <Footer />
