5d67af644cd825535ac1fd36028b5cb0637f5226
redfyel <lianis.ee7@gmail.com>
2024-11-25 09:49:43 +0530

    auth errors

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -7,7 +7,7 @@ function Register({ onRegisterSuccess }) {
 
   async function onUserRegister(newUser) {
     try {
-      let res = await fetch(`https://academiaconnect-x5a6.onrender.com`, {
+      let res = await fetch(`https://academiaconnect-x5a6.onrender.com/user-api/user`, {
         method: "POST",
         headers: { "Content-type": "application/json" },
         body: JSON.stringify(newUser),
